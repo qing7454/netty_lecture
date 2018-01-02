fc8b44e85aaee1e418a9a01cca917b2ec8c75534
fanrenqing <qing7454@163.com>
2018-01-02 09:19:13 +0800

    initialize project

---
+++ src/main/java/com/qing/netty/first/TestServerInitializer.java
@@ -0,0 +1,15 @@
+package com.qing.netty.first;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+public class TestServerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline channelPipeline = ch.pipeline();
+        channelPipeline.addLast("httpServerCodec", new HttpServerCodec());
+        channelPipeline.addLast("testHttpServerHandler", new TestHttpServerHandler());
+    }
+}
